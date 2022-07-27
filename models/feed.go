c6ad14f26cdd4345a719fdf18291086f10ede95e
RadCraftplay <radcraftplay2@gmail.com>
2022-07-27 23:08:37 +0200

    Fixed nullability database data problem

--- models/feed.go
+++ models/feed.go
@@ -3,7 +3,7 @@ package models
 type Feed struct {
 	ID     uint   `json:"id" gorm:"primary_key"`
 	URL    string `json:"url"`
-	FeedId uint   `json:"feed_id"`
+	FeedId *uint  `json:"feed_id"`
 }
 
 func (Feed) TableName() string {
