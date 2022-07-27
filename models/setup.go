d43d7c6dc406120d80dd157f0c182bbeb83f9bcc
RadCraftplay <radcraftplay2@gmail.com>
2022-07-27 19:02:07 +0200

    Updated setup.go to use postgres

--- models/setup.go
+++ models/setup.go
@@ -1,20 +1,33 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+const (
+	host     = "localhost"
+	port     = 5432
+	username = "febri"
+	password = "e3b3440172abd558bbd535eefbc36512a0a574b5360bc4d59d14fc9404dc8bbc"
+	dbname   = "febri"
+)
+
 func ConnectDatabase() {
-	database, err := gorm.Open("sqlite3", "test.db")
+	database, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable", host, username, password, dbname, port),
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database!")
 	}
 
-	database.AutoMigrate(&Book{})
+	database.AutoMigrate(&Feed{})
 
 	DB = database
 }
