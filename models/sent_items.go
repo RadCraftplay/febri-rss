46e118a1fb20d8e81ffb8a5466e7b37c82274977
RadCraftplay <radcraftplay2@gmail.com>
2022-07-28 15:04:08 +0200

    SentItems id is nullable to allow default values

--- models/sent_items.go
+++ models/sent_items.go
@@ -1,7 +1,7 @@
 package models
 
 type SentItems struct {
-	ID      uint   `json:"id" gorm:"primary_key"`
+	ID      *uint  `json:"id" gorm:"primary_key"`
 	Feed_ID uint   `json:"feed_id"`
 	GUID    string `json:"guid"`
 }
