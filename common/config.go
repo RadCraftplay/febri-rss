354fb6aeb8144c886e12f388353dc67c00cf2934
RadCraftplay <radcraftplay2@gmail.com>
2022-08-24 14:39:50 +0200

    Added config.go

---
+++ common/config.go
@@ -0,0 +1,74 @@
+package common
+
+import (
+	"io/ioutil"
+	"os"
+	"time"
+
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	configFilename = "./config"
+)
+
+func getDefaultConfiguration() FebriRssConfiguration {
+	return FebriRssConfiguration{
+		struct {
+			FetchRss struct {
+				period time.Duration "yaml:\"period\""
+			}
+		}{
+			FetchRss: struct {
+				period time.Duration "yaml:\"period\""
+			}{
+				period: time.Hour * 6,
+			},
+		},
+	}
+}
+
+func saveConfiguration(configuration FebriRssConfiguration) {
+	out, err := yaml.Marshal(&configuration)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create(configFilename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	n, err := file.Write(out)
+	if err != nil || n != len(out) {
+		panic("Unable to properly write configuration!")
+	}
+}
+
+func LoadConfiguration() FebriRssConfiguration {
+	var configuration FebriRssConfiguration
+
+	if _, err := os.Stat(configFilename); os.IsNotExist(err) {
+		saveConfiguration(getDefaultConfiguration())
+	}
+
+	content, err := ioutil.ReadFile(configFilename)
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(content, &configuration)
+	if err != nil {
+		panic(err)
+	}
+	return configuration
+}
+
+type FebriRssConfiguration struct {
+	Services struct {
+		FetchRss struct {
+			period time.Duration `yaml:"period"`
+		}
+	}
+}
