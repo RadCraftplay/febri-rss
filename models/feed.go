47976330ba00063412b129f3339d48e26293e40d
RadCraftplay <radcraftplay2@gmail.com>
2022-11-19 15:49:37 +0100

    Altered feed model

--- models/feed.go
+++ models/feed.go
@@ -7,8 +7,8 @@ import (
 )
 
 type Feed struct {
-	ID          uint      `json:"id" gorm:"primary_key"`
-	URL         string    `json:"url"`
-	SourceId    uuid.UUID `json:"source_id"`
-	LastUpdated time.Time `json:"last_updated" gorm:"default:now()"`
+	ID          uint       `json:"id" gorm:"primary_key"`
+	URL         string     `json:"url"`
+	SourceId    uuid.UUID  `json:"source_id"`
+	LastUpdated *time.Time `json:"last_updated" gorm:"default:now()"`
 }
